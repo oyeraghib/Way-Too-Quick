3be75a6494a64cfc55bfe5d3eeee123e3aafd89b
oyeraghib <raghib.khan76@gmail.com>
2022-05-26 20:34:58 +0530

    add adapter for Category Items

---
+++ app/src/main/java/com/example/waytooquick/adapters/CategoryAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.waytooquick.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.waytooquick.databinding.ListItemCatogoriesBinding
+import com.example.waytooquick.models.Category
+
+class CategoryAdapter :
+    ListAdapter<Category, CategoryAdapter.CategoryAdapterViewHolder>(CategoryDiffCallback()) {
+
+    class CategoryAdapterViewHolder(val binding: ListItemCatogoriesBinding) : RecyclerView.ViewHolder(binding.root) {}
+
+    class CategoryDiffCallback: DiffUtil.ItemCallback<Category>() {
+        override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+            return oldItem == newItem
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+            return oldItem === newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryAdapterViewHolder {
+        val inflater = parent.context.getSystemService(LayoutInflater::class.java)
+        val binding = ListItemCatogoriesBinding.inflate(inflater, parent, false)
+
+        return CategoryAdapterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryAdapterViewHolder, position: Int) {
+        val category = getItem(position)
+
+        holder.binding.tvCategoryLabel.text = category.name
+        holder.binding.ivCategoryItem.load(category.icon)
+    }
+
+}
---
+++ app/src/main/java/com/example/waytooquick/models/Category.kt
@@ -0,0 +1,9 @@
+package com.example.waytooquick.models
+
+class Category(
+    var name: String,
+    var icon: String,
+    var color: String,
+    var brief: String,
+    var id: Int
+)
